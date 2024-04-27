abc6500e64396a2e0efb6741519f89b12051c6b8
zain <zainjhere@gmail.com>
2024-04-27 08:47:25 +0500

    Integrate SQS service

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -3,19 +3,26 @@ import AWS from "aws-sdk";
 const ses = new AWS.SES({region: 'eu-west-1'});
 
 async function sendMail(event, context) {
+  const record = event.Records[0];
+
+  console.log("Record processing", record);
+  const email = JSON.parse(record.body);
+
+  const {subject, body, recipient} = email;
+
   const params = {
-    Source: 'auction-service@axioutech.com',
+    Source: 'mail@zainthedev.com',
     Destination: {
-      ToAddresses: ['auction-service@axioutech.com'],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: 'Hello from Auction Service!',
+          Data: body,
         },
       },
       Subject: {
-        Data: 'Test Mail',
+        Data: subject,
       },
     },
   };
